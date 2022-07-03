1effed34fea0a63388743e7ef460745c070cea73
Michal Kolaczkowski <michu@pop-os.localdomain>
2022-07-03 22:17:51 +0200

    fixed events fetching

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import styles from "./Layout.module.css";
 import { getEvents } from "../../firebase/service/events";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { setEvent } from "../../redux/reducer/counterSlice";
 
@@ -11,8 +11,11 @@ const Layout = ({ children, pageLoading }) => {
   const getEvent = (data) => {
     dispatch(setEvent(data));
   };
+  const events = useSelector((state) => state.event.events);
   useEffect(() => {
-    getEvents(getEvent);
+    if (events.length === 0) {
+      getEvents(getEvent);
+    }
   });
   return (
     <div className="content">
