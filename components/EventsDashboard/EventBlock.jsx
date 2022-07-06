ea249f423a4f02fadd77b924f2b8a319b3ab199d
Michal Kolaczkowski <michu@pop-os.localdomain>
2022-07-06 22:22:44 +0200

    styling improvements, added wemanifest

--- components/EventsDashboard/EventBlock.jsx
+++ components/EventsDashboard/EventBlock.jsx
@@ -3,6 +3,7 @@ import styles from "./EventBlock.module.css";
 import { setCurrentEvent } from "../../redux/reducer/counterSlice";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/dist/client/router";
+import Image from "next/image";
 
 const EventBlock = ({ event }) => {
   const dispatch = useDispatch();
@@ -17,7 +18,16 @@ const EventBlock = ({ event }) => {
       className={styles.blockContainer}
       onClick={() => handleRedirect(event)}
     >
-      <img className={styles.blockImage} src={event.logoURL} />
+      <div className={styles.logoContainer}>
+        <Image
+          src={event.logoURL}
+          alt="logo"
+          width={100}
+          height={100}
+          style={{ borderRadius: 12 }}
+        />
+      </div>
+      {/* <img className={styles.blockImage} src={event.logoURL} /> */}
       <div className={styles.blockInfo}>
         <h1 className={styles.eventTitle}>{event.name}</h1>
         <div className={styles.blockRow}>
--- pages/_app.js
+++ pages/_app.js
@@ -5,24 +5,17 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [pageLoading, setPageLoading] = useState(false);
 
-  useEffect(() => {
-    const handleStart = () => {
-      setPageLoading(true);
-    };
-    const handleFinish = () => {
-      setPageLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleFinish);
-  }, [router]);
   return (
     <Provider store={store}>
+      <Head>
+        <title>BreakingEventsHub</title>
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       {router.pathname === "/login" || router.pathname === "/register" ? (
         <Component {...pageProps} />
       ) : (
